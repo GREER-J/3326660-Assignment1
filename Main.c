13f02f3278c33873afd3038f1b92b5d41f094c3e
Grigri217 <jmlgreer@gmail.com>
2019-04-03 14:55:33 +1100

    Added an encode() function
    This functions builds upon tranlate and implements the math required to encode the ceaser cipher. Wrap around test have been conducted and were passed.
    
    The translate function will still go into an infinite loop if passed a ascii code of something not between a:z. This will be fixed at a later date.

--- Main.c
+++ Main.c
@@ -2,22 +2,27 @@
 
 //Prototype
 char translate(char *alphabet, char letter);
+char encode(char *alphabet, char letter, char key);
 
 //Main
 int main(){
 
 char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-char letter = 122; 
+char letter = 97; //97 = a, 122 = z (for testing)
 char * pAlphabet = &alphabet;
+char key = 5;
 
-char position = translate(pAlphabet, letter);
-printf("%d: %c", position, alphabet[position]);
+char test = encode(pAlphabet, letter, key);
+
+printf("%c", alphabet[test]);
 
 return 0;
 }
 
 
+//************************************************
+char translate(char *alphabet, char letter){
 /*
 AIM: to translate a given letter from ascii into an internal alphabet from which all calculations will be made. This will make the flow easier.
 
@@ -25,8 +30,6 @@ Arguments:
     -This function accepts a pointer to a list of type char, which contains the alphabet that the program will use.
     -It also accepts a letter of type char, which it will match inside the alphabet.
 */
-char translate(char *alphabet, char letter){
-
     //Will infinite loop if passed value outide of a:z
     
     //To ensure the while loop always fires
@@ -42,4 +45,11 @@ char translate(char *alphabet, char letter){
         }
     }
     return(position);
+}
+
+//************************************************
+char encode(char *alphabet, char letter, char key){
+    char position = translate(alphabet, letter);
+    char encoded_position = (position + key)%26;
+    return encoded_position;
 }
